ff2363c2570f3c25c885e50b46adeb581377ae52
VizDiablo <79822447+VizDiablo@users.noreply.github.com>
2022-06-02 10:54:36 -0600

    After lesson commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return <div />;
+  const [count, setCount] = useState(0);
+
+  function increment() {
+    setCount(count + 1);
+  }
+
+  function decrement() {
+    setCount(count - 1);
+  }
+
+  function resetCount() {
+    setCount(0);
+  }
+
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+      <div>
+        <button onClick={increment}>+</button>
+        <button onClick={decrement}>-</button>
+      </div>
+      <button onClick={resetCount}>R</button>
+    </div>
+  );
 }
 
 export default App;
